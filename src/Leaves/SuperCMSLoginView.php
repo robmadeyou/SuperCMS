74488c81cf1e847c827bc007a971bd1569020227
robmadeyou <robmadeyou@gmail.com>
2016-08-18 14:18:09 +0100

    Pretty login form

--- src/Leaves/SuperCMSLoginView.php
+++ src/Leaves/SuperCMSLoginView.php
@@ -6,37 +6,50 @@ use Rhubarb\Scaffolds\Authentication\Leaves\LoginView;
 
 class SuperCMSLoginView extends LoginView
 {
+    public function createSubLeaves()
+    {
+        parent::createSubLeaves();
+
+        $this->leaves['username']->addCssClassNames('form-control');
+        $this->leaves['password']->addCssClassNames('form-control');
+        $this->leaves['Login']->addCssClassNames('btn btn-default');
+    }
+
     public function printViewContent()
     {
         print <<<HTML
-        <form class="form-horizontal">
-          <div class="form-group">
-            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-10">
-              {$this->leaves['username']}
+        <div id="login-container" class=" row">
+            <div class="col-md-3 col-md-offset-5">
+                <form class="form-horizontal">
+                  <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-4 control-label">Email</label>
+                    <div class="col-sm-8">
+                      {$this->leaves['username']}
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-4 control-label">Password</label>
+                    <div class="col-sm-8">
+                      {$this->leaves['password']}
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> Remember me
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-10">
+                      {$this->leaves['Login']}
+                    </div>
+                  </div>
+                </form>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-            <div class="col-sm-10">
-              {$this->leaves['password']}
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              <div class="checkbox">
-                <label>
-                  <input type="checkbox"> Remember me
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              {$this->leaves['Login']}
-            </div>
-          </div>
-        </form>
+        </div>
 HTML;
     }
 }
