eb9278ab8ae7f67344f94b9861e1b02d186a79d4
robmadeyou <robmadeyou@gmail.com>
2016-08-25 14:02:44 +0100

    Starting to add a bit more layout to the page, need to re-think how to access categories

--- src/Layouts/DefaultLayout.php
+++ src/Layouts/DefaultLayout.php
@@ -3,7 +3,8 @@
 namespace SuperCMS\Layouts;
 
 use Rhubarb\Crown\Html\ResourceLoader;
-use Rhubarb\Patterns\Layouts\BaseLayout;
+use Rhubarb\Stem\Filters\Equals;
+use SuperCMS\Models\Product\Category;
 
 class DefaultLayout extends SuperCMSDefaultLayout
 {
@@ -21,18 +22,56 @@ class DefaultLayout extends SuperCMSDefaultLayout
             <div class="col-md-10 col-md-offset-1 content">
                 <div id="top-banner">
                     <div class="top-banner-bottom-buttons">
-                        <div class="pull-left btn-group">
-                            <a href="/" class="btn btn-default">Home</a>
-                            <a href="/about-us/" class="btn btn-default round-right">About us</a>
-                        </div>
-                        <div class="pull-right btn-group">
-                            <a href="/news/" class="btn btn-default round-left">Latest News</a>
-                            <a href="/contact/" class="btn btn-default">Contact Us</a>
-                        </div>
+                        <? $this->printBannerButtons() ?>
                     </div>
                     <div class="banner-border"></div>
                 </div>
+                <div class="category-list">
+                    <? $this->printCategoryMenu() ?>
+                </div>
                 <div id="content">
+        <?php
+    }
+
+    protected function printCategoryMenu()
+    {
+        ?>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <?php
+                    foreach (Category::find(new Equals('ParentCategoryID', 0)) as $category) {
+                        $children = '';
+                        foreach ($category->ChildCategories as $childCategory) {
+
+                        }
+
+                        if ($children) {
+                            $category .= <<<HTML
+                            <div class="dropdown-menu">{$children}</div>
+HTML;
+                        }
+    
+                        print <<<HTML
+                        <li class="dropdown"><a href="/category/{$category->SeoSafeName}/">{$category->Name}</a></li>
+HTML;
+                    }
+                ?>
+            </ul>
+        </div>
+        <?php
+    }
+
+    protected function printBannerButtons()
+    {
+        ?>
+        <div class="pull-left btn-group">
+            <a href="/" class="btn btn-default">Home</a>
+            <a href="/about-us/" class="btn btn-default round-right">About us</a>
+        </div>
+        <div class="pull-right btn-group">
+            <a href="/news/" class="btn btn-default round-left">Latest News</a>
+            <a href="/contact/" class="btn btn-default">Contact Us</a>
+        </div>
         <?php
     }
 
