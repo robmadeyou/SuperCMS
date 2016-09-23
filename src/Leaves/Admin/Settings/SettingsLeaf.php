10bf4d7a9713b3879967fec53bec06f4e40294c2
robmadeyou <robmadeyou@gmail.com>
2016-09-24 00:48:24 +0100

    too tired, will contine

---
+++ src/Leaves/Admin/Settings/SettingsLeaf.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SuperCMS\Leaves\Admin\Settings;
+
+use Rhubarb\Leaf\Leaves\Leaf;
+
+class SettingsLeaf extends Leaf
+{
+    protected function getViewClass()
+    {
+    }
+
+    protected function createModel()
+    {
+    }
+}
