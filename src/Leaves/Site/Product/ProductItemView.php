1a1386c46da87082d0f07179e8934986f46fa903
rojr <robmadeyou@gmail.com>
2016-10-19 23:39:29 +0100

    Starting product view page

--- src/Leaves/Site/Product/ProductItemView.php
+++ src/Leaves/Site/Product/ProductItemView.php
@@ -4,6 +4,7 @@ namespace SuperCMS\Leaves\Site\Product;
 
 use Rhubarb\Leaf\Controls\Common\Buttons\Button;
 use Rhubarb\Leaf\Views\View;
+use SuperCMS\Models\Product\Product;
 
 class ProductItemView extends View
 {
@@ -19,7 +20,36 @@ class ProductItemView extends View
 
     protected function printViewContent()
     {
-        print $this->model->restModel->Name;
+        $product = $this->model->restModel;
         print $this->leaves['AddToBasket'];
+
+        $this->printProductTitle($product);
+        ?>
+        <div class="row">
+            <div class="col-sm-5 c-product-image-section">
+                <?php $this->printProductImages($product); ?>
+            </div>
+            <div class="col-sm-7">
+                <?php $this->printProductDescription($product); ?>
+            </div>
+        </div>
+        <?php
+    }
+
+    protected function printProductTitle(Product $product)
+    {
+        print $product->Name;
+    }
+
+    protected function printProductImages(Product $product)
+    {
+        ?>
+        <img class="main-image" src="<?=$product->getDefaultImage()?>">
+        <?php
+    }
+
+    protected function printProductDescription(Product $product)
+    {
+        print $product->Description;
     }
 }
