58f1a3a84550141057018845f8c3d3ab483262f0
robmadeyou <robmadeyou@gmail.com>
2017-08-07 19:26:56 +0100

    Adding validation for the signup step

--- src/Controls/LocationPicker/LocationPickerView.php
+++ src/Controls/LocationPicker/LocationPickerView.php
@@ -2,12 +2,10 @@
 
 namespace SuperCMS\Controls\LocationPicker;
 
-use Rhubarb\Leaf\Controls\Common\SelectionControls\DropDown\DropDown;
 use Rhubarb\Leaf\Controls\Common\Text\TextBox;
 use Rhubarb\Leaf\Leaves\Controls\ControlView;
 use Rhubarb\Leaf\Leaves\LeafDeploymentPackage;
 use SuperCMS\Controls\HtmlButton\HtmlButton;
-use SuperCMS\Models\Shopping\Basket;
 use SuperCMS\Models\User\Location;
 use SuperCMS\Models\User\SuperCMSUser;
 
@@ -23,17 +21,17 @@ class LocationPickerView extends ControlView
         parent::createSubLeaves();
 
         $this->registerSubLeaf(
-            new TextBox( 'AddressLine1' ),
-            new TextBox( 'AddressLine2' ),
-            new TextBox( 'Town' ),
-            new TextBox( 'PostCode' ),
-            new TextBox( 'PhoneNumber' ),
+            new TextBox('AddressLine1'),
+            new TextBox('AddressLine2'),
+            new TextBox('Town'),
+            new TextBox('PostCode'),
+            new TextBox('PhoneNumber'),
             $country = new TextBox('Country'),
-            $save = new HtmlButton( 'Save', 'Save', function () {
+            $save = new HtmlButton('Save', 'Save', function () {
                 $this->model->saveEvent->raise();
             }, true),
-            $cancel = new HtmlButton( 'Cancel', 'Cancel', function () {
-            }, true )
+            $cancel = new HtmlButton('Cancel', 'Cancel', function () {
+            }, true)
         );
 
         $save->setButtonType('submit');
@@ -41,12 +39,12 @@ class LocationPickerView extends ControlView
 
         foreach ($this->leaves as $leaf) {
             if ($leaf instanceof TextBox) {
-                $leaf->addCssClassNames( 'form-control' );
+                $leaf->addCssClassNames('form-control');
             }
         }
 
-        $save->addCssClassNames( 'btn', 'button', 'button-checkout' );
-        $cancel->addCssClassNames( 'btn', 'button', 'button-checkout', 'button--secondary' );
+        $save->addCssClassNames('btn', 'button', 'button-checkout');
+        $cancel->addCssClassNames('btn', 'button', 'button-checkout', 'button--secondary');
     }
 
     protected function printViewContent()
@@ -58,8 +56,8 @@ class LocationPickerView extends ControlView
     protected function printAddressList()
     {
         print '<div class="row">';
-        foreach (SuperCMSUser::getUserLocations() as $location) {
-            $this->printAddress( $location );
+        foreach (SuperCMSUser::getUserLocations()->addSort('LocationID') as $location) {
+            $this->printAddress($location);
         }
         print '</div>';
     }
@@ -109,7 +107,7 @@ HTML;
                     <div class="modal-body">
                         <form>
                             <div class="form-group">
-                                <label>Address Line 1</label>
+                                <label>Address Line 1 <span class="required">*</span></label>
                                 <?= $this->leaves[ 'AddressLine1' ] ?>
                             </div>
                             <div class="form-group">
@@ -117,15 +115,15 @@ HTML;
                                 <?= $this->leaves[ 'AddressLine2' ] ?>
                             </div>
                             <div class="form-group">
-                                <label>Town</label>
+                                <label>Town <span class="required">*</span></label>
                                 <?= $this->leaves[ 'Town' ] ?>
                             </div>
                             <div class="form-group">
-                                <label>Post Code</label>
+                                <label>Post Code <span class="required">*</span></label>
                                 <?= $this->leaves[ 'PostCode' ] ?>
                             </div>
                             <div class="form-group">
-                                <label>Country</label>
+                                <label>Country <span class="required">*</span></label>
                                 <?= $this->leaves[ 'Country' ] ?>
                             </div>
                             <div class="form-group">
--- src/Controls/LocationPicker/LocationPickerViewBridge.js
+++ src/Controls/LocationPicker/LocationPickerViewBridge.js
@@ -1,95 +1,119 @@
 var bridge = function (leafPath) {
-	window.rhubarb.viewBridgeClasses.ViewBridge.apply(this, arguments);
+    window.rhubarb.viewBridgeClasses.ViewBridge.apply(this, arguments);
 };
 
 bridge.prototype = new window.rhubarb.viewBridgeClasses.ViewBridge();
 bridge.prototype.constructor = bridge;
 
 bridge.prototype.attachEvents = function () {
-	var self = this;
-	this.AddressLine1 = this.findChildViewBridge('AddressLine1');
-	this.AddressLine2 = this.findChildViewBridge('AddressLine2');
-	this.Town = this.findChildViewBridge('Town');
-	this.PostCode = this.findChildViewBridge('PostCode');
-	this.Country = this.findChildViewBridge('Country');
-	this.PhoneNumber = this.findChildViewBridge('PhoneNumber');
-	this.cancel = this.findChildViewBridge('Cancel');
-	this.save = this.findChildViewBridge('Save');
-	this.modal = $('.modal-location-edit');
+    var self = this;
+    this.AddressLine1 = this.findChildViewBridge('AddressLine1');
+    this.AddressLine2 = this.findChildViewBridge('AddressLine2');
+    this.Town = this.findChildViewBridge('Town');
+    this.PostCode = this.findChildViewBridge('PostCode');
+    this.Country = this.findChildViewBridge('Country');
+    this.PhoneNumber = this.findChildViewBridge('PhoneNumber');
+    this.cancel = this.findChildViewBridge('Cancel');
+    this.save = this.findChildViewBridge('Save');
+    this.modal = $('.modal-location-edit');
 
-	$('.js-location-edit').click(function(){
-		var current = $(this);
+    $('.js-location-edit').click(function(){
+        var current = $(this);
 
-		self.model.selectedLocation = current.data('id');
-		self.saveState();
+        self.model.selectedLocation = current.data('id');
+        self.saveState();
 
-		self.loadEditData();
-	});
+        self.loadEditData();
+    });
 
-	$('.js-location-delete').click(function(event){
-		var current = $(this);
+    $('.js-location-delete').click(function(event){
+        var current = $(this);
 
-		self.model.selectedLocation = current.data('id');
-		self.saveState();
+        self.model.selectedLocation = current.data('id');
+        self.saveState();
 
-		self.viewNode.classList.add('ajax-progress');
-		self.raiseServerEvent('delete', function() {
-			self.viewNode.classList.remove('ajax-progress');
-		});
+        self.viewNode.classList.add('ajax-progress');
+        self.raiseServerEvent('delete', function() {
+            self.viewNode.classList.remove('ajax-progress');
+        });
 
-		event.preventDefault();
-		return false;
-	});
+        event.preventDefault();
+        return false;
+    });
 
-	$('.js-add-location').click(function() {
-		self.model.selectedLocation = 0;
-		self.saveState();
+    $('.js-add-location').click(function() {
+        self.model.selectedLocation = 0;
+        self.saveState();
 
-		self.loadEditData();
-	});
+        self.loadEditData();
+    });
 
-	$('.js-select-location-item').click(function(event) {
-		event.preventDefault();
-		var current = $(this).closest('.js-location-item');
-		self.raiseServerEvent('selectLocation', current.data('id'), function() {
-			$('.js-location-item').removeClass('selected');
-			current.addClass('selected');
-		});
-	});
+    $('.js-select-location-item').click(function(event) {
+        event.preventDefault();
+        var current = $(this).closest('.js-location-item');
+        self.raiseServerEvent('selectLocation', current.data('id'), function() {
+            $('.js-location-item').removeClass('selected');
+            current.addClass('selected');
+        });
+    });
 
-	this.cancel.viewNode.onclick = this.closeModal;
+    this.cancel.viewNode.onclick = this.closeModal;
 
-	this.save.attachClientEventHandler('OnButtonPressed', function(){
-		self.viewNode.classList.add('ajax-progress');
-		self.closeModal();
-	});
+    this.save.attachClientEventHandler('OnButtonPressed', function(event) {
+        if (self.validate()) {
+            self.viewNode.classList.add('ajax-progress');
+            self.closeModal();
+        } else {
+            return false;
+        }
+    });
 
-	this.save.attachClientEventHandler('ButtonPressCompleted', function(){
-		self.viewNode.classList.remove('ajax-progress');
-
-	});
+    this.save.attachClientEventHandler('ButtonPressCompleted', function(){
+        self.viewNode.classList.remove('ajax-progress');
+    });
 };
 
 bridge.prototype.loadEditData = function() {
-	var self = this;
+    var self = this;
 
-	var modal = this.modal.find('.modal-content');
-	modal.addClass('ajax-progress');
-	this.raiseServerEvent('loadData', function(data) {
-		if (data) {
-			self.AddressLine1.setValue(data.AddressLine1);
-			self.AddressLine2.setValue(data.AddressLine2);
-			self.Town.setValue(data.Town);
-			self.PostCode.setValue(data.PostCode);
-			self.Country.setValue(data.Country);
-			self.PhoneNumber.setValue(data.PhoneNumber);
-		}
-		modal.removeClass('ajax-progress');
-	});
+    var modal = this.modal.find('.modal-content');
+    modal.addClass('ajax-progress');
+    this.raiseServerEvent('loadData', function(data) {
+        if (data) {
+            self.AddressLine1.setValue(data.AddressLine1);
+            self.AddressLine2.setValue(data.AddressLine2);
+            self.Town.setValue(data.Town);
+            self.PostCode.setValue(data.PostCode);
+            self.Country.setValue(data.Country);
+            self.PhoneNumber.setValue(data.PhoneNumber);
+        }
+        modal.removeClass('ajax-progress');
+    });
+};
+
+bridge.prototype.validate = function() {
+    var fieldsToValidate = [
+        'AddressLine1',
+        'Town',
+        'PostCode',
+        'Country'
+    ];
+
+    var success = true;
+
+    for (var i = 0; i < fieldsToValidate.length; i++) {
+        var input = eval('this.' + fieldsToValidate[i]);
+        if (!input.getValue()) {
+            $(input.viewNode).closest('.form-group').addClass('has-error');
+            $(input.viewNode).closest('.form-group').find('.required').append(' <span>This field is required</span>');
+            success = false;
+        }
+    }
+    return success;
 };
 
 bridge.prototype.closeModal = function() {
-	$('.modal-location-edit').modal('hide');
+    $('.modal-location-edit').modal('hide');
 };
 
 window.rhubarb.viewBridgeClasses.LocationPickerViewBridge = bridge;
