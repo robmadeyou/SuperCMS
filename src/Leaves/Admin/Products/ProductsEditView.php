ca60b30450080625a9b8473d95ca3732ffe817b8
rojr <robmadeyou@gmail.com>
2017-11-03 23:34:18 +0000

    Removing inputs from the product edit view and moving to separate contrl

--- src/Leaves/Admin/Products/ProductsEditView.php
+++ src/Leaves/Admin/Products/ProductsEditView.php
@@ -14,6 +14,7 @@ use SuperCMS\Controls\HtmlEditor\HtmlEditor;
 use SuperCMS\Controls\KeyValue\KeyValue;
 use SuperCMS\Controls\Shipping\ShippingMultiSelection;
 use SuperCMS\Controls\ToggleSwitch\ToggleSwitch;
+use SuperCMS\Leaves\Admin\Products\ProductVariations\ProductVariations;
 use SuperCMS\Models\Product\ProductImage;
 use SuperCMS\Models\Product\ProductVariation;
 use SuperCMS\Views\SuperCMSCrudView;
@@ -31,33 +32,18 @@ class ProductsEditView extends SuperCMSCrudView
 
         $this->registerSubLeaf(
             new TextBox('Name'),
-            new TextBox('VariationName'),
             new HtmlEditor('Description'),
-            new HtmlEditor('VariationDescription'),
-            new TextBox('Price'),
-            new TextBox('AmountAvailable'),
             new CategoryDropdown('CategoryID'),
             $relatedProducts = new ChosenMultiSelect('RelatedProductIDs'),
-            $imageUpload = new Dropzone('ImageUpload'),
             $properties = new KeyValue('Properties'),
             new ShippingMultiSelection('ShippingTypes'),
-            $toggleSwitch = new ToggleSwitch('Live')
+            $toggleSwitch = new ToggleSwitch('Live'),
+            $variationsEdit = new ProductVariations($this->model->restModel, 'Variations')
         );
 
         $toggleSwitch->addHtmlAttribute('tooltip', 'Set the Product live or not');
 
         $properties->setInputClasses(['form-control']);
-
-        $imageUpload->fileUploadedEvent->attachHandler(function ($data) {
-            ProductImage::createImageForProduct(new ProductVariation($_GET['variation']), $data);
-        });
-
-        $imageUpload->deleteImageEvent->attachHandler(function ($url) {
-            $image = ProductImage::findFirst(new Equals('ImagePath', $url));
-            $image->delete();
-        });
-
-
 
         $this->bootstrapInputs();
     }
@@ -66,13 +52,6 @@ class ProductsEditView extends SuperCMSCrudView
     {
         $settings = HtmlPageSettings::singleton();
         $settings->pageTitle = 'Editing Product';
-
-        $images = [];
-        foreach (ProductImage::find(new Equals('ProductVariationID', $this->model->selectedVariation->UniqueIdentifier))->addSort('Priority') as $image) {
-            $images[] = new DropzoneUploadedFileDetails($image->ProductImageID, $image->ImagePath, $image->UniqueIdentifier);
-        }
-        $this->leaves['ImageUpload']->setUploadedFiles($images);
-        $this->leaves['ImageUpload']->setPostParams('?variation=' . $this->model->selectedVariation->UniqueIdentifier);
 
         $this->printFieldset(
             '',
@@ -86,44 +65,8 @@ class ProductsEditView extends SuperCMSCrudView
         );
         ?>
         <div class="input-group-breaker"></div>
-        <ul class="nav nav-pills">
-            <?php
-            foreach ($this->model->restModel->Variations as $variation) {
-                $class = ( $variation->UniqueIdentifier == $this->model->selectedVariation->UniqueIdentifier ? 'active nav-bar-tabs-first' : '' );
-                print '<li role="presentation" class="' . $class . ' product-list-tabs" ><a href="#" class="product-variation-tab" data-id="' . $variation->UniqueIdentifier . '">' . $variation->Name . '  <span class="delete-variation"><i class="fa fa-times fa-1x" aria-hidden="true"></i></span></a></li>';
-            }
-            ?>
-            <li role="presentation" class="product-list-tabs" id="tab-add-button"><p>&nbsp;&nbsp;<i class="fa fa-plus" aria-hidden="true"></i></span></p></li>
-        </ul>
-        <form>
-            <div class="form-group">
-                <label>Name</label>
-                <?= $this->leaves[ 'VariationName' ] ?>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label>Price</label>
-                        <?= $this->leaves[ 'Price' ] ?>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label>Amount Available</label>
-                        <?= $this->leaves[ 'AmountAvailable' ] ?>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <label>Short Description</label>
-                <?= $this->leaves[ 'VariationDescription' ] ?>
-            </div>
-            <div class="form-group">
-                <label>Images</label>
-                <?= $this->leaves[ 'ImageUpload' ] ?>
-            </div>
-        </form>
         <?php
+        print $this->leaves['Variations'];
     }
 
     protected function printLeftButtons()
