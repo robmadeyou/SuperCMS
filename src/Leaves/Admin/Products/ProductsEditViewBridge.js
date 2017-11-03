ad54321d94aad7567ca99e6b2c17e019a2dc9aa3
rojr <robmadeyou@gmail.com>
2017-11-03 23:34:44 +0000

    Detaching javascript events from event edit viewbridge

--- src/Leaves/Admin/Products/ProductsEditViewBridge.js
+++ src/Leaves/Admin/Products/ProductsEditViewBridge.js
@@ -9,37 +9,11 @@ bridge.prototype.attachEvents = function () {
 	var firstTab = $('.nav-bar-tabs-first');
 	var self = this;
 
-	$('.product-variation-tab').click(function(event) {
-		$(this).closest('.form-body').addClass('ajax-progress');
-		if (!event.target.classList.contains('fa')) {
-			var lastSelected = $('.product-list-tabs.active a').data('id');
-			changeTab($(this).parent());
-
-			self.raiseServerEvent('ChangeProductVariation', lastSelected, $(this).data('id'));
-			event.preventDefault();
-			return false;
-		} else {
-			if (confirm('Are you sure you want to remove this variation?')) {
-				self.raiseServerEvent('VariationDelete', $(this).data('id'));
-			}
-			event.preventDefault();
-			event.stopPropagation();
-			return false;
-		}
-
-	});
-
 	$('#tab-add-button').click(function(){
 		var lastSelected = $('.product-list-tabs.active a').data('id');
 
 		self.raiseServerEvent('AddNewProduct', lastSelected);
 	});
-
-	function changeTab(tab) {
-		$('#' + this.leaftPath + ' .active').removeClass('active');
-
-		tab.addClass('active');
-	}
 
 	$('#' + this.leafPath + '_VariationName').keyup(function(){
 		firstTab.find('a').html($(this).val() + '<span class="delete-variation"><i class="fa fa-times fa-1x" aria-hidden="true"></i></span>');
