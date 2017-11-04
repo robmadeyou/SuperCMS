61e7bba1f52abae6f4f112932139b73e61ce22a8
robmadeyou <robmadeyou@gmail.com>
2017-11-04 20:42:00 +0000

    Remove child function already existing in parent

--- src/Controls/Dropzone/DropzoneViewBridge.js
+++ src/Controls/Dropzone/DropzoneViewBridge.js
@@ -72,37 +72,6 @@ scms.create('DropzoneViewBridge', function(){
                 timeout = setTimeout(refreshGridly, 50);
             });
             refreshGridly();
-        },
-		findEventHost:function(){
-            var selfNode = document.getElementById(this.leafPath);
-
-            while (selfNode) {
-                var testNode = selfNode;
-
-                selfNode = selfNode.parentNode;
-
-                var className = ( testNode.className ) ? testNode.className : "";
-
-                if (className.indexOf("event-host") == 0 || className.indexOf("event-host") > 0) {
-                    if (!testNode.viewBridge) {
-                        if (!testNode.id) {
-                            testNode.id = "event-host";
-                        }
-
-                        new window.ViewBridge(testNode.id);
-
-                        if ((testNode.className.indexOf("event-host") == 0 || testNode.className.indexOf("event-host") > 0) && testNode.viewBridge != undefined) {
-                            testNode.viewBridge.host = true;
-                        }
-                    }
-                }
-
-                if (testNode.viewBridge && testNode.viewBridge.host && testNode.className.indexOf("configured") == -1) {
-                    return testNode.viewBridge;
-                }
-            }
-
-            return false;
-		}
+        }
 	}
 });
