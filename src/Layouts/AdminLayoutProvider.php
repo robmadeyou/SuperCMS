a13b5d7121e21716736af574ac29ae97ef6ee5ab
robmadeyou <robmadeyou@gmail.com>
2017-11-11 21:55:56 +0000

    New Empty layout provider

---
+++ src/Layouts/AdminLayoutProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SuperCMS\Layouts;
+
+use Rhubarb\Leaf\LayoutProviders\LayoutProvider;
+
+class AdminLayoutProvider extends LayoutProvider
+{
+    public function printItemsWithContainer($containerTitle, ...$items)
+    {
+    }
+
+    public function printContainerTitle($containerTitle)
+    {
+    }
+
+    public function printLabelValuePairs($pairs)
+    {
+    }
+
+    public function printValueWithLabel($value, $label)
+    {
+    }
+
+    public function printLabel($label)
+    {
+    }
+
+    public function printValue($value)
+    {
+    }
+}
